1d12a7a67282c6eaf51f123d585fe5ffdd6af8ea
huangm26 <huangm26@gmail.com>
2014-03-09 16:45:34 -0500

    法规及额外io

--- src/run/Main.java
+++ src/run/Main.java
@@ -1,9 +1,36 @@
 package run;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+import weka.core.Instances;
+
 public class Main {
 	
-	public static void main(String args[])
+	public void readData()
+	{
+		try
+		{
+			BufferedReader br = new BufferedReader(new FileReader("/User/huangm26/git/cs440-hw4/bin/DataSet1.arff"));
+		}
+		catch (FileNotFoundException e)
+		{
+			System.out.println(e);
+		}
+		catch (IOException e1)
+		{
+			System.out.println(e1);
+		}
+	}
+	
+	public static void main(String[] args)
 	{
 		
+	
 	}
+
 }
